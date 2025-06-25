7dcac6972e9657fa79d168aa395d8436545e8d47
spcotoon <springcomicskr@gmail.com>
2025-06-26 04:24:24 +0900

    project commit

---
+++ src/main/java/com/spcotoon/speeddrawing/SpeeddrawingApplication.java
@@ -0,0 +1,13 @@
+package com.spcotoon.speeddrawing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeeddrawingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeeddrawingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spcotoon/speeddrawing/SpeeddrawingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spcotoon.speeddrawing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeeddrawingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
